f02fff0b1a5afc50dc892d07dfafc661378577c5
VINÍCIUS LOPES <vinicius_lopes150@estudante.sesisenai.org.br>
2026-07-16 10:43:19 -0300

    feat: initializr the project

---
+++ src/test/java/com/weg/Manutencao_API/ManutencaoApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.weg.Manutencao_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManutencaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
