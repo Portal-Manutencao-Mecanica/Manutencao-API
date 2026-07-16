f2ad2b839bf7af9d37f76d9aabf2b5b0ad18d62b
VINÍCIUS LOPES <vinicius_lopes150@estudante.sesisenai.org.br>
2026-07-16 13:41:58 -0300

    fix(user): correct user

--- src/main/java/com/weg/Manutencao_API/user/entity/User.java
+++ src/main/java/com/weg/Manutencao_API/user/entity/User.java
@@ -1,21 +1,17 @@
-package com.weg.Manutencao_API.usuario.entity;
+package com.weg.Manutencao_API.user.entity;
 
 import com.weg.Manutencao_API.enums.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "user")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    private Long id;
     @Column(name = "user_name")
     private String name;
     @Column(name = "user_email")
@@ -30,9 +26,6 @@ public class User {
         this.email = email;
         this.role = role;
         this.createdAt = LocalDateTime.now();
-    }
-
-    public User() {
     }
 
 }
