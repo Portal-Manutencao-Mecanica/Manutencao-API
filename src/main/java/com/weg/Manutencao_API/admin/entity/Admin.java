4d28b14c74ae3d4714118585d595823d41ea708a
DAVI DE SOUZA <davi_de-souza@estudante.sesisenai.org.br>
2026-07-16 14:17:05 -0300

    fix(Admin) put entity and table notations

--- src/main/java/com/weg/Manutencao_API/admin/entity/Admin.java
+++ src/main/java/com/weg/Manutencao_API/admin/entity/Admin.java
@@ -4,16 +4,20 @@ import com.weg.Manutencao_API.enums.Role;
 import com.weg.Manutencao_API.usuario.entity.User;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "admin")
 public class Admin extends User {
 
     @Id
