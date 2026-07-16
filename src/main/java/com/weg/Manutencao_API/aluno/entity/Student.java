4fcbb2ff053ab52401c29e659a93af891aaddaee
DAVI DE SOUZA <davi_de-souza@estudante.sesisenai.org.br>
2026-07-16 14:04:25 -0300

    feat(Student) create student

---
+++ src/main/java/com/weg/Manutencao_API/aluno/entity/Student.java
@@ -0,0 +1,31 @@
+package com.weg.Manutencao_API.aluno.entity;
+
+import com.weg.Manutencao_API.enums.Role;
+import com.weg.Manutencao_API.usuario.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "student")
+public class Student extends User{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private Long id;
+
+    public Student(String name, String email, Role role) {
+        super(name, email, role);
+    }
+}
