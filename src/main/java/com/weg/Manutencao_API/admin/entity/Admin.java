de0d62176cd43233be5d9143313130302bfbd273
DAVI DE SOUZA <davi_de-souza@estudante.sesisenai.org.br>
2026-07-16 14:01:11 -0300

    feat(Admin) create admin

--- src/main/java/com/weg/Manutencao_API/admin/entity/Admin.java
+++ src/main/java/com/weg/Manutencao_API/admin/entity/Admin.java
@@ -1,5 +1,27 @@
 package com.weg.Manutencao_API.admin.entity;
 
-public class Admin {
-    
+import com.weg.Manutencao_API.enums.Role;
+import com.weg.Manutencao_API.usuario.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Admin extends User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
+    private Long id;
+
+    public Admin(String name, String email, Role role) {
+        super(name, email, role);
+    }
 }
