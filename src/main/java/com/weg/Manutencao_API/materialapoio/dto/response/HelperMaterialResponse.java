f06a1613c79deba6219d4d0a94e7bd9d7e948b84
Junior Antunes <jugabriel2612@gmail.com>
2026-07-17 09:21:13 -0300

    feat(HelperMaterial): create HelperMaterial Dto(Response)

--- src/main/java/com/weg/Manutencao_API/materialapoio/dto/response/HelperMaterialResponse.java
+++ src/main/java/com/weg/Manutencao_API/materialapoio/dto/response/HelperMaterialResponse.java
@@ -1,5 +1,10 @@
 package com.weg.Manutencao_API.materialapoio.dto.response;
 
-public record HelperMaterialResponse() {
+public record HelperMaterialResponse(
+        Long id,
+        String linkTecnic,
+        String linkLubrification,
+        String linkPreventMaintance,
+        String linkManual) {
 
 }
