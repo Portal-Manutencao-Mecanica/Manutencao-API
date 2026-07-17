5094e98158365349f418747854d0986844d288f5
DAVI DE SOUZA <davi_de-souza@estudante.sesisenai.org.br>
2026-07-17 09:11:54 -0300

    fix(Admin) add id to the AdminDtoResponse

--- src/main/java/com/weg/Manutencao_API/admin/dto/response/AdminResponseDto.java
+++ src/main/java/com/weg/Manutencao_API/admin/dto/response/AdminResponseDto.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import com.weg.Manutencao_API.enums.Role;
 
 public record AdminResponseDto(
+        Long id,
         String name,
         String email,
         Role role,
