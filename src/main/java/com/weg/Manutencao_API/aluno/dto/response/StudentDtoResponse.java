6be8b34b02152ceab780dc788cf6cbc965c73089
DAVI DE SOUZA <davi_de-souza@estudante.sesisenai.org.br>
2026-07-17 09:11:14 -0300

    fix(Student) add id to the StudentDtoResponse

--- src/main/java/com/weg/Manutencao_API/aluno/dto/response/StudentDtoResponse.java
+++ src/main/java/com/weg/Manutencao_API/aluno/dto/response/StudentDtoResponse.java
@@ -7,6 +7,7 @@ import com.weg.Manutencao_API.enums.Role;
 import com.weg.Manutencao_API.turma.entity.ClassGroup;
 
 public record StudentDtoResponse(
+        Long id,
         String nome,
         String email,
         Role role,
