0afc7e3bb4ed1993900442dfc408c5dfe115dcb9
DAVI DE SOUZA <davi_de-souza@estudante.sesisenai.org.br>
2026-07-17 09:07:21 -0300

    feat(Student) create StudentResponseDto and StudentRequestDto for Student

---
+++ src/main/java/com/weg/Manutencao_API/aluno/dto/response/StudentDtoResponse.java
@@ -0,0 +1,16 @@
+package com.weg.Manutencao_API.aluno.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.weg.Manutencao_API.enums.Role;
+import com.weg.Manutencao_API.turma.entity.ClassGroup;
+
+public record StudentDtoResponse(
+        String nome,
+        String email,
+        Role role,
+        List<ClassGroup> classGroups,
+        LocalDateTime createdAt) {
+
+}
