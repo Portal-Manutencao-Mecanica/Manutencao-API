90438c11487721e753004021776cc58a583902d8
Junior Antunes <jugabriel2612@gmail.com>
2026-07-20 10:51:41 -0300

    feat(Place) : create controller of Place

---
+++ src/main/java/com/weg/Maintenance_API/place/controller/PlaceController.java
@@ -0,0 +1,52 @@
+package com.weg.Maintenance_API.place.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.weg.Maintenance_API.maintenancerequest.dto.request.MaintenanceRequestRequest;
+import com.weg.Maintenance_API.maintenancerequest.dto.response.MaintenanceRequestResponse;
+import com.weg.Maintenance_API.place.dto.request.PlaceRequest;
+import com.weg.Maintenance_API.place.dto.response.PlaceResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/lugar")
+@RequiredArgsConstructor
+public class PlaceController {
+
+     @PostMapping()
+    public ResponseEntity<PlaceResponse> create(@RequestBody PlaceRequest entity) {
+        return null;
+    }
+
+    @GetMapping()
+    public ResponseEntity<PlaceResponse> getAll() {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PlaceResponse> getById(@PathVariable Long id) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PlaceResponse> update(@PathVariable Long id,
+            @RequestBody PlaceRequest entity) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        return null;
+    }
+
+    
+}
