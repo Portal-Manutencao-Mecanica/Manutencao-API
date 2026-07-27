15ab5f3649593dae60199e074abc21687ae2b961
DAVI DE SOUZA <davi_de-souza@estudante.sesisenai.org.br>
2026-07-27 12:55:10 -0300

    feat(Machine) create DerivedQuery findAll for search all the pages

--- src/main/java/com/weg/Maintenance_API/machine/repository/MachineRepository.java
+++ src/main/java/com/weg/Maintenance_API/machine/repository/MachineRepository.java
@@ -1,5 +1,7 @@
 package com.weg.Maintenance_API.machine.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import com.weg.Maintenance_API.machine.entity.Machine;
 @Repository
 public interface MachineRepository extends JpaRepository<Machine, Long>{
     
+    Page<Machine> findAll(Pageable pageable);
 }
 
 
--- src/main/java/com/weg/Maintenance_API/machine/service/MachineService.java
+++ src/main/java/com/weg/Maintenance_API/machine/service/MachineService.java
@@ -2,6 +2,8 @@ package com.weg.Maintenance_API.machine.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -73,4 +75,5 @@ public class MachineService {
     public void delete(Long id) {
         machineRepository.deleteById(id);
     }
+
 }
