c0b9013ee390ec41aefdcf9383365769f8417fc3
VINÍCIUS LOPES <vinicius_lopes150@estudante.sesisenai.org.br>
2026-08-06 09:55:29 -0300

    refactor: consolidar schema inicial

--- src/main/resources/db/migration/V6__allow_maintenance_request_images.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE public.media
-    DROP CONSTRAINT IF EXISTS media_storage_key_key;
-
-ALTER TABLE public.media
-    ALTER COLUMN storage_key TYPE TEXT;
--- src/main/resources/db/migration/V7__add_equipment_identifiers.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE public.equipment
-    ADD COLUMN equipment_patrimony character varying(100),
-    ADD COLUMN equipment_tag character varying(100);
